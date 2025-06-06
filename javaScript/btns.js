cf236e949b1590440717e801cea41215b1345a39
Lucas silva <134072600+LucasSilvaDev1@users.noreply.github.com>
2025-06-06 10:28:58 -0300

    botão "busca", progamado

--- javaScript/btns.js
+++ javaScript/btns.js
@@ -1,13 +1,25 @@
 
+const search = document.getElementById('search')
 
-function windowSearch() {
-    let search = document.getElementById('search')
+const busca = document.getElementById('busca').addEventListener('click', function () {
+    search.style.display = 'block'
+    search.focus(search)
+
+})
+
+
+search.addEventListener('focus', () => {
     if (search.classList.contains('off')) {
         search.classList.replace('off', 'on')
         search.style.display = 'block'
-        search.focus(search)
-    } else {
+    }
+})
+search.addEventListener('blur', () => {
+    if (search.classList.contains('on')) {
         search.classList.replace('on', 'off')
         search.style.display = 'none'
     }
-}
+})
+
+
+
